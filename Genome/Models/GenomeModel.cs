14d32bca5c443b09b65aa6ec6bc708656e0ba8e1
Taylor Flatt <taylorjflatt@gmail.com>
2016-03-03 16:07:12 -0600

    Added three user classes. We are using static classes because we know there will only need to be three roles. They are Unverified, Verified, and Admin.
    Restricted the website to only verified users.
    
    Provided redirections based on roles to the approriate informational pages.
    
    Still need to remove the ability to add new roles. Decided to refrain from too many ambitious programming decisions.

---
+++ Genome/Models/GenomeModel.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Genome.Models
+{
+    public class GenomeModel
+    {
+        [Key]
+        public int uuid { get; set; }
+
+        [Required]
+        [Display(Name = "Masurca K-Mer Value")]
+        public int MasurcaKMerValue { get; set; }
+
+        [Required]
+        [Display(Name = "Masurca CPU Thread Count")]
+        public int MasurcaThreadNum { get; set; }
+
+        [Required]
+        [Display(Name = "Masurca Jellyfish hash size (10x size of genome)")]
+        public int MasurcaJellyfishHashSize { get; set; }
+
+        [Required]
+        [Display(Name = "Masurca Linking Mates (binary)")]
+        public int MasurcaLinkingMates { get; set; }
+
+        [Required]
+        [Display(Name = "Masurca Limit Jump Coverage")]
+        public bool MasurcaLimitJumpCoverage { get; set; }
+
+        [Required]
+        [Display(Name = "Masurca Soap Assembly")]
+        public bool MasurcaSoapAssembly { get; set; }
+    }
+}
