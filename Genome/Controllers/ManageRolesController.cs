17bd140341e56cabe5f33e7494c9c4fd504f16f0
Taylor Flatt <taylorjflatt@gmail.com>
2016-03-03 20:43:09 -0600

    When changing roles of a user (as an admin), I made the roles mutually exclusive which cleaned up a decent amount of code.
    Removed the option of creating user roles.
    
    Cleaned up the Manage view.
    
    Added some input validation on the forms and moved the error display to the top of the page and made it much more obvious.
    
    TODO: I plan to revisit the layout of the Manage Roles to make it a bit more fluid and easier to use.

--- Genome/Controllers/ManageRolesController.cs
+++ Genome/Controllers/ManageRolesController.cs
@@ -1,14 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Data.Entity;
 using System.Linq;
-using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Genome.Models;
 using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
 
 namespace Genome.Controllers
@@ -27,134 +23,77 @@ namespace Genome.Controllers
 
         }
         //
-        // GET: /Roles/
+        // GET: /ManageRoles/
         public ActionResult Index()
-        {
-            var roles = context.Roles.ToList();
-            return View(roles);
-        }
-
-        //
-        // GET: /Roles/Create
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        //
-        // POST: /Roles/Create
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                context.Roles.Add(new Microsoft.AspNet.Identity.EntityFramework.IdentityRole()
-                {
-                    Name = collection["RoleName"]
-                });
-                context.SaveChanges();
-                ViewBag.ResultMessage = "Role created successfully !";
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /Roles/Edit/5
-        public ActionResult Edit(string roleName)
-        {
-            var thisRole = context.Roles.Where(r => r.Name.Equals(roleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
-
-            return View(thisRole);
-        }
-
-        //
-        // POST: /Roles/Edit/5
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit(Microsoft.AspNet.Identity.EntityFramework.IdentityRole role)
-        {
-            try
-            {
-                context.Entry(role).State = System.Data.Entity.EntityState.Modified;
-                context.SaveChanges();
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /Roles/Delete/5
-        public ActionResult Delete(string RoleName)
-        {
-            var thisRole = context.Roles.Where(r => r.Name.Equals(RoleName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
-            context.Roles.Remove(thisRole);
-            context.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
-        public ActionResult ManageUserRoles()
         {
             var list = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
             ViewBag.Roles = list;
 
-            var userList = context.Users.OrderBy(u => u.UserName).ToList().Select(rr => new SelectListItem { Value = rr.UserName.ToString(), Text = rr.UserName }).ToList();
-            ViewBag.UserNames = userList;
+            //var userList = context.Users.OrderBy(u => u.UserName).ToList().Select(rr => new SelectListItem { Value = rr.UserName.ToString(), Text = rr.UserName }).ToList();
+            //ViewBag.UserNames = userList;
 
             return View();
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult RoleAddToUser(string UserName, string RoleName)
+        public ActionResult AddUserToRole(string UserName, string RoleName)
         {
-            //If they don't select a role for the user and leave it as the default, notify them to change it and return the view.
-            if(RoleName.Equals("Select..."))
-            {
-                ViewBag.RoleSelectError = "Please select a proper role.";
-                return View("ManageUserRoles");
-            }
+            //Make sure valid information is entered/selected.
+            RoleChangeValidation(RoleName, UserName);
 
+            //Admin > Verified > Unverified. The roles are mutually exclusive.
             ApplicationUser user = context.Users.FirstOrDefault(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase));
-            if (user != null) UserManager.AddToRole(user.Id, RoleName);
+            if (user != null)
+            {
+                if (UserManager.GetRoles(user.Id).Contains("Admin"))
+                    UserManager.RemoveFromRoles(user.Id, "Admin");
 
-            ViewBag.ResultMessage = "Role created successfully !";
+                else if (UserManager.GetRoles(user.Id).Contains("Verified"))
+                    UserManager.RemoveFromRoles(user.Id, "Verified");
+
+                else if (UserManager.GetRoles(user.Id).Contains("Unverified"))
+                    UserManager.RemoveFromRoles(user.Id, "Unverified");
+
+                UserManager.AddToRole(user.Id, RoleName);
+            }
+            
+            ViewBag.ResultMessage = "User added to role successfully !";
 
             // prepopulat roles for the view dropdown
             var list = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
             ViewBag.Roles = list;
 
-            return View("ManageUserRoles");
+            return View("Index");
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult GetRoles(string UserName)
         {
+            //Make sure valid information is entered/selected.
+            RoleChangeValidation(UserName);
+
             if (!string.IsNullOrWhiteSpace(UserName))
             {
                 ApplicationUser user = context.Users.FirstOrDefault(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase));
                 if (user != null) ViewBag.RolesForThisUser = UserManager.GetRoles(user.Id);
 
-                // prepopulat roles for the view dropdown
+                // Populate the roles for a dropdown.
                 var list = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
                 ViewBag.Roles = list;
             }
 
-            return View("ManageUserRoles");
+            return View("Index");
         }
 
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteRoleForUser(string UserName, string RoleName)
         {
+            //Make sure valid information is entered/selected.
+            RoleChangeValidation(UserName, RoleName);
+
             ApplicationUser user = context.Users.FirstOrDefault(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase));
 
             if (UserManager.IsInRole(user.Id, RoleName))
@@ -162,15 +101,44 @@ namespace Genome.Controllers
                 UserManager.RemoveFromRole(user.Id, RoleName);
                 ViewBag.ResultMessage = "Role removed from this user successfully !";
             }
+
             else
             {
                 ViewBag.ResultMessage = "This user doesn't belong to selected role.";
             }
-            // prepopulat roles for the view dropdown
+
+            // Populate the roles for a dropdown.
             var list = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
             ViewBag.Roles = list;
 
-            return View("ManageUserRoles");
+            return View("Index");
+        }
+
+        //private void RoleChangeValidation(string UserName)
+        //{
+        //    //If they don't select a username for the user and leave it as the default, notify them to change it and return the view.
+        //    if (UserName.Equals(""))
+        //    {
+        //        ViewBag.UserSelectError = "Please select a user role.";
+
+        //        var temp_list = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+        //        ViewBag.Roles = temp_list;
+        //    }
+        //}
+
+        private void RoleChangeValidation(string UserName, string RoleName = "null")
+        {
+            //If they don't select a username for the user and leave it as the default, notify them to change it and return the view.
+            if (UserName.Equals(""))
+                ViewBag.UserSelectError = "Please select a user role.";
+
+            //If they don't select a role for the user and leave it as the default, notify them to change it and return the view.
+            if (RoleName.Equals(""))
+                ViewBag.RoleSelectError = "Please select a proper role.";
+
+            var temp = context.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+
+            ViewBag.Roles = temp;
         }
 
         protected override void Dispose(bool disposing)
