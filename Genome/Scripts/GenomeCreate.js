84d4c6d75620320f6c32ce57ac7dbacb773ba462
Taylor Flatt <taylorjflatt@gmail.com>
2016-03-26 23:57:03 -0500

    Added validation for step 2 with warning messages.
    Looked into regex to validate URLs. I may end up just doing very basic stuff such as making sure it starts with www. or http(s)://. I can't find any decent regex that works 100% of the time for all possible cases.  At least nothing I trust enough to put into production. Maybe passing the burden off to the user might be the best bet. Just throw a warning in to double check the URL.

--- Genome/Scripts/GenomeCreate.js
+++ Genome/Scripts/GenomeCreate.js
@@ -11,9 +11,52 @@ function Step2MoveBack(){
 }
 
 function Step2MoveForward() {
-    var DataURL = document.getElementById("")
-    document.getElementById('Step2').style.display = "none";
-    document.getElementById('Step3').style.display = "block";
+    var invalidData = 0;
+
+    // Paired-end read validation is only done if paired-end reads is checked.
+    if (document.getElementById("PEReads").checked)
+    {
+        var input = document.getElementById("PELengthInput").value;
+
+        // Make sure the value of the box is between 0 and 100. (TEST - MAY REMOVE) (WORKS)
+        if(input < 0 || input > 100)
+        {
+            document.getElementById("PELengthErrorMsg").innerHTML = "The value you entered was invalid. You may only enter numbers from 0-100!"
+            invalidData++;
+        }
+    }
+
+    // Jump read validation is only done if Jump reads is checked.
+    if (document.getElementById("JumpReads").checked)
+    {
+        var input = document.getElementById("JumpLengthInput");
+
+        // Make sure the value of the box is between 0 and 100. (TEST - MAY REMOVE) (WORKS)
+        if (input < 0 || input > 100)
+        {
+            document.getElementById("JumpLengthErrorMsg").innerHTML = "The value you entered was invalid. You may only enter numbers from 0-100!"
+            invalidData++;
+        }
+    }
+
+
+    if (invalidData == 0)
+    {
+        // Reset the error warnings.
+        document.getElementById("PELengthErrorMsg").innerHTML = ""
+        document.getElementById("JumpLengthErrorMsg").innerHTML = ""
+        document.getElementById("WizardErrors").innerHTML = ""
+
+        // Move to the next step.
+        document.getElementById('Step2').style.display = "none";
+        document.getElementById('Step3').style.display = "block";
+    }
+
+    else
+    {
+        // Write an error message at the top of the page indicating that one or more fields are incorrect.
+        document.getElementById("WizardErrors").innerHTML = "Please correct the errors before proceeding!"
+    }
 }
 
 function Step3MoveBack(){
@@ -28,6 +71,13 @@ function Step3MoveForward() {
 
 function appendURLBox(){
     $('#addUrlBtn').append("<br ><input type='text' id='url_"+ ++x +"' value=''>");
+}
+
+// This function needs tweeked. It will return valid for something like www.google.comd
+function isURL(url) {
+    var pattern = new RegExp(/^(?:(?:https?|ftp):\/\/)?(?:www\.)(?:\S+(?::\S*)?@)?(?:(?!10(?:\.\d{1,3}){3})(?!127(?:\.\d{1,3}){3})(?!169\.254(?:\.\d{1,3}){2})(?!192\.168(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]+-?)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/[^\s]*)?$/i);
+
+    return pattern.test(url);
 }
 
 function concatURLs() {
