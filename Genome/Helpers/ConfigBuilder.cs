f9b47aa279bc3e58a630c73cff6a92f867ff4188
martygrawe <martygrawe@gmail.com>
2016-03-28 00:21:37 -0500

    All 3 config files are generated. MasurcaConfig appears to be completely finished. InitConfig can be edited with more commands easily. SchedulerConfig just has a test line writing to the file because I'm not sure what that should look like yet.

--- Genome/Helpers/ConfigBuilder.cs
+++ Genome/Helpers/ConfigBuilder.cs
@@ -7,68 +7,73 @@ namespace Genome.Helpers
 {
     public class ConfigBuilder
     {
-        //private string BuildMasurcaConfig(int jobNumber, GenomeModel genomeModel, string dataLocation1, string dataLocation2 = "")
-        //{
-        //    string path = @"~/AssemberConfigs/Job" + jobNumber;
-        //    Directory.CreateDirectory(path);
-        //    string fileName = "MasurcaConfig.txt";
-        //    string fullPath = path + fileName;
-
-        //    if (!File.Exists(fullPath))
-        //    {
-        //        File.Create(fullPath);
-        //        TextWriter tw = new StreamWriter(fullPath);
-        //        //tw.WriteLine("The very first line!");
-
-
-        //        tw.WriteLine("DATA");
-        //        //data params
-        //        if (genomeModel.Step2.PEReads)
-        //            tw.WriteLine("PE= pe " + genomeModel.Step2.PairedEndLength + " 20  " + dataLocation1 + "  " + dataLocation2);
-
-        //        // These locations are not the same, used for now for development purposes
-        //        if (genomeModel.Step2.JumpReads)
-        //            tw.WriteLine("JUMP= sh " + genomeModel.Step2.JumpLength + " 200  " + dataLocation1 + "  " + dataLocation2);
-
-        //        tw.WriteLine("END");
-
-
-        //        tw.WriteLine("PARAMETERS");
-        //        // Parameter params
-        //        tw.WriteLine("GRAPH_KMER_SIZE = auto");
-        //        tw.WriteLine("USE_LINKING_MATES = " + Convert.ToInt32(genomeModel.Step3.MasurcaLinkingMates));
-        //        tw.WriteLine("LIMIT_JUMP_COVERAGE = " + genomeModel.Step3.MasurcaLimitJumpCoverage);
-
-        //        if (genomeModel.Step3.MasurcaCAParameters)
-        //            tw.WriteLine("CA_PARAMETERS = cgwErrorRate=0.25 ovlMemory=4GB");
-
-        //        else
-        //            tw.WriteLine("CA_PARAMETERS = cgwErrorRate=0.15 ovlMemory=4GB");
-
-        //        // Minimum count k-mers used in error correction 1 means all k-mers are used.  one can increase to 2 if coverage >100
-        //        tw.WriteLine("KMER_COUNT_THRESHOLD = 1");
-        //        tw.WriteLine("NUM_THREADS = " + genomeModel.Step3.MasurcaThreadNum);
-        //        // this is mandatory jellyfish hash size -- a safe value is estimated_genome_size*estimated_coverage
-        //        tw.WriteLine("JF_SIZE = " + genomeModel.Step3.MasurcaJellyfishHashSize);
-        //        tw.WriteLine("DO_HOMOPOLYMER_TRIM = " + genomeModel.Step3.HomoTrim);
-        //        tw.WriteLine("END");
-
-        //        tw.Close();
-        //    }
-
-        //    return fullPath;
-        //}
-
-        public void InitConfig(GenomeModel genomeModel, List<string> dataSource)
+        public string BuildMasurcaConfig(GenomeModel genomeModel, List<string> dataSource)
         {
-            string path = @"~/AssemberConfigs/Job" + genomeModel.uuid + "/";
+            string path = AppDomain.CurrentDomain.BaseDirectory + "AssemblerConfigs\\" + "Job" + genomeModel.uuid + "\\";
             Directory.CreateDirectory(path);
-            string fileName = "init_config_" + genomeModel.uuid + ".txt";
+            string fileName = "MasurcaConfig_" + genomeModel.uuid + ".txt";
             string fullPath = path + fileName;
 
             if (!File.Exists(fullPath))
             {
-                File.Create(fullPath);
+                TextWriter tw = new StreamWriter(fullPath);
+
+                tw.WriteLine("DATA");
+                string dataString = "";
+                foreach (string url in dataSource)
+                {
+                    dataString = dataString + url + " ";
+                }
+                //data params
+                if (genomeModel.PEReads)
+                {
+                    tw.WriteLine("PE= pe " + genomeModel.PairedEndLength + " 20  " + dataString);
+                }
+
+                // These locations are not the same, used for now for development purposes
+                if (genomeModel.JumpReads)
+                {
+                    tw.WriteLine("JUMP= sh " + genomeModel.JumpLength + " 200  " + dataString);
+                }
+
+                tw.WriteLine("END");
+
+
+                tw.WriteLine("PARAMETERS");
+                // Parameter params
+                tw.WriteLine("GRAPH_KMER_SIZE = auto");
+                tw.WriteLine("USE_LINKING_MATES = " + Convert.ToInt32(genomeModel.MasurcaLinkingMates));
+                tw.WriteLine("LIMIT_JUMP_COVERAGE = " + genomeModel.MasurcaLimitJumpCoverage);
+
+                if (genomeModel.MasurcaCAParameters)
+                    tw.WriteLine("CA_PARAMETERS = cgwErrorRate=0.25 ovlMemory=4GB");
+
+                else
+                    tw.WriteLine("CA_PARAMETERS = cgwErrorRate=0.15 ovlMemory=4GB");
+
+                // Minimum count k-mers used in error correction 1 means all k-mers are used.  one can increase to 2 if coverage >100
+                tw.WriteLine("KMER_COUNT_THRESHOLD = 1");
+                tw.WriteLine("NUM_THREADS = " + genomeModel.MasurcaThreadNum);
+                // this is mandatory jellyfish hash size -- a safe value is estimated_genome_size*estimated_coverage
+                tw.WriteLine("JF_SIZE = " + genomeModel.MasurcaJellyfishHashSize);
+                tw.WriteLine("DO_HOMOPOLYMER_TRIM = " + genomeModel.HomoTrim);
+                tw.WriteLine("END");
+
+                tw.Close();
+            }
+
+            return fullPath;
+        }
+
+        public string BuildInitConfig(GenomeModel genomeModel, List<string> dataSource)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "AssemblerConfigs\\" + "Job" + genomeModel.uuid + "\\";
+            Directory.CreateDirectory(path);
+            string fileName = "init_" + genomeModel.uuid + ".sh";
+            string fullPath = path + fileName;
+
+            if (!File.Exists(fullPath))
+            {
                 TextWriter tw = new StreamWriter(fullPath);
 
                 tw.WriteLine("cd " + "WORKING DIRECTORY/Data"); // Change directory to working directory
@@ -83,13 +88,30 @@ namespace Genome.Helpers
 
 
                 }
-
+                tw.Close();
 
                 // Next step is to do wget error checking. 
 
-
-                //tw.WriteLine("The very first line!");
             }
+            return fullPath;
+        }
+
+        public string BuildSchedulerConfig(GenomeModel genomeModel)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "AssemblerConfigs\\" + "Job" + genomeModel.uuid + "\\";
+            Directory.CreateDirectory(path);
+            string fileName = "Scheduler_" + genomeModel.uuid + ".sh";
+            string fullPath = path + fileName;
+
+            if (!File.Exists(fullPath))
+            {
+                TextWriter tw = new StreamWriter(fullPath);
+
+                tw.WriteLine("Test Line");
+
+                tw.Close();
+            }
+            return fullPath;
         }
     }
 }
