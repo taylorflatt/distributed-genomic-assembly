87b4b570948098e1dc07787d0425b0017497d4e7
Taylor Flatt <taylorjflatt@gmail.com>
2016-03-27 23:44:59 -0500

    Added more functionality that will reflect a more realistic environment.
    Cleaned up some of the commands as well by passing in parameters that are more aptly named.
    
    TODO: May still need to change the CWD from the logon node. That has yet to be tested.

--- Genome/Helpers/SSHConfig.cs
+++ Genome/Helpers/SSHConfig.cs
@@ -2,7 +2,6 @@
 using Genome.Models;
 
 /// <summary>
-/// TODO: Check permissions on newly downloaded files - may need to make them executable.
 /// TODO: May need to change the CWD to a default folder and run the scripts from a special folder rather than the user's folder.
 /// </summary>
 namespace Genome.Helpers
@@ -12,17 +11,16 @@ namespace Genome.Helpers
         private string userName = "";
         private string password = "";
         private string ip = "";
-        private GenomeModel model;
+        private GenomeModel genomeModel;
 
         private static string COMPUTENODE1 = "compute-0-24";
         private static string COMPUTENODE2 = "compute-0-25";
 
-        public SSHConfig(string ip, string userName, string password, GenomeModel genomeModel)
+        public SSHConfig(string ip, string userName, string password)
         {
             this.userName = userName;
             this.password = password;
             this.ip = ip;
-            model = genomeModel;
 
             CreateConnection();
         }
@@ -39,17 +37,71 @@ namespace Genome.Helpers
             // The init.sh script will contain all the basic logic to download the data and initiate the job on the assembler(s).
             using (var sshClient = new SshClient(ip, userName, password))
             {
+                string jobID = genomeModel.uuid.ToString();
+                string jobDirectory = "Job/ " + jobID;
+
                 sshClient.Connect();
 
-                UploadInitScript(sshClient);
-                UploadMasurcaScript(sshClient);
-                UploadSchedulerScript(sshClient);
+                CreateDirectories(sshClient, jobDirectory);
+
+                UploadInitScript(sshClient, jobID);
+                UploadMasurcaScript(sshClient, jobID);
+                UploadSchedulerScript(sshClient, jobID);
+
+                ChangePermissions(sshClient, jobDirectory);
+
                 AddJobToScheduler(sshClient);
 
                 sshClient.Disconnect();
             }
         }
 
+        // Create the job directory.
+        private void CreateDirectories(SshClient client, string jobDirectory)
+        {
+            using (var cmd = client.CreateCommand("mkdir -p" + jobDirectory))
+            {
+                cmd.Execute();
+            }
+        }
+
+        // Upload the init script (initializes all other assembler scripts and grabs data at runtime).
+        private void UploadInitScript(SshClient client, string jobID)
+        {
+            using (var cmd = client.CreateCommand("wget [DOMAIN/IP]/AssemblerConfigs/Job" + jobID + "/init" + jobID + ".sh"))
+            {
+                cmd.Execute();
+            }
+        }
+
+        // Upload the Masurca assembler script.
+        private void UploadMasurcaScript(SshClient client, string jobID)
+        {
+            using (var cmd = client.CreateCommand("wget [DOMAIN/IP]/AssemblerConfigs/Job" + jobID + "/masurca" + jobID + ".sh"))
+            {
+                cmd.Execute();
+            }
+        }
+
+        // Upload the scheduler script.
+        private void UploadSchedulerScript(SshClient client, string jobID)
+        {
+            using (var cmd = client.CreateCommand("wget [DOMAIN/IP]/AssemblerConfigs/Job" + jobID + "/scheduler" + jobID + ".sh"))
+            {
+                cmd.Execute();
+            }
+        }
+
+        // Change the entire directory's contents to R/W/X.
+        private void ChangePermissions(SshClient client, string jobDirectory)
+        {
+            using (var cmd = client.CreateCommand("chmod -R +rwx " + jobDirectory))
+            {
+                cmd.Execute();
+            }
+        }
+
+        // Add the job to the scheduler.
         private void AddJobToScheduler(SshClient client)
         {
             using (var cmd = client.CreateCommand("./scheduler.sh"))
@@ -57,90 +109,5 @@ namespace Genome.Helpers
                 cmd.Execute();
             }
         }
-
-        private void UploadInitScript(SshClient client)
-        {
-            using (var cmd = client.CreateCommand("wget [DOMAIN/IP]/AssemblerConfigs/Job" + model.uuid + "/init" + model.uuid + ".sh"))
-            {
-                cmd.Execute();
-            }
-        }
-
-        private void UploadMasurcaScript(SshClient client)
-        {
-            using (var cmd = client.CreateCommand("wget [DOMAIN/IP]/AssemblerConfigs/Job" + model.uuid + "/masurca" + model.uuid + ".sh"))
-            {
-                cmd.Execute();
-            }
-        }
-
-        private void UploadSchedulerScript(SshClient client)
-        {
-            using (var cmd = client.CreateCommand("wget [DOMAIN/IP]/AssemblerConfigs/Job" + model.uuid + "/scheduler" + model.uuid + ".sh"))
-            {
-                cmd.Execute();
-            }
-        }
-
-        //public SSHConfig(string configUrl, string schedulerUrl, string dataUrl)
-        //{
-        //    this.configUrl = configUrl;
-        //    this.schedulerUrl = schedulerUrl;
-        //    this.dataUrl = dataUrl;
-        //}
-
-        //public void CreateConnectionThread()
-        //{
-        //    //ThreadStart job = new ThreadStart();
-        //    Thread thread = new Thread(() => CreateConnection(configUrl, schedulerUrl, dataUrl));
-        //    //thread.IsBackground = true;
-        //    thread.Start();
-        //}
-
-        //private void CreateConnection(List<string> configUrl, string schedulerUrl, List<string> dataUrl)
-        //{
-        //    using (var sshClient = new SshClient("ip", "username", "password"))
-        //    {
-        //        sshClient.Connect();
-        //        using (var cmd = sshClient.CreateCommand("mkdir -p /tmp/uploadtest && chmod +rw /tmp/uploadtest"))
-        //        {
-        //            cmd.Execute();
-        //            Console.WriteLine("Command>" + cmd.CommandText);
-        //            Console.WriteLine("Return Value = {0}", cmd.ExitStatus);
-        //        }
-
-        //        // May not be needed once scheduler is running
-        //        sshClient.CreateCommand("ssh " + COMPUTENODE1);
-
-        //        foreach (string config in configUrl)
-        //        {
-        //            sshClient.CreateCommand("wget \"" + config + "\"").Execute();
-        //        }
-
-        //        //Create working directory
-        //        //Need to make a directory: mkdir //share/GenomeAssembly/{username}/{job#}
-        //        sshClient.CreateCommand();
-
-
-        //  The following will be changed with an init script on big dog itself 
-        //        for (int i = 0; i < dataUrl.Count; i++)
-        //        {
-        //            // Last command sent starts the assembler specifc commands
-        //            if (i == dataUrl.Count - 1)
-        //                sshClient.CreateCommand("wget \"" + dataUrl[i] + "\" && ").Execute();
-
-        //            else
-        //                sshClient.CreateCommand("wget \"" + dataUrl[i] + "\"").Execute();
-        //        }
-
-        //        sshClient.CreateCommand("wget " + schedulerUrl).Execute();
-
-        //        sshClient.Disconnect();
-        //    }
     }
 }
-
-    //Config File
-    //Data
-    //Scheduler config
-    //Execute assembler specific commands
