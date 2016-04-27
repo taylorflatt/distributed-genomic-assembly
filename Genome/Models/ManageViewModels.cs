e561c8a3a4cc127e827de8b6e941a1e0642cc90d
Taylor Flatt <taylorjflatt@gmail.com>
2016-04-27 13:05:22 -0500

    Added the ability view the number of submitted jobs along with a couple statistics in the manage.

--- Genome/Models/ManageViewModels.cs
+++ Genome/Models/ManageViewModels.cs
@@ -9,6 +9,7 @@ namespace Genome.Models
     {
         public bool HasPassword { get; set; }
         public IList<UserLoginInfo> Logins { get; set; }
+        public bool ClusterAccountVerified { get; set; }
         public string PhoneNumber { get; set; }
         public bool TwoFactor { get; set; }
         public bool BrowserRemembered { get; set; }
